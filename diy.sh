d5cc16bb9c81751ab845f7ecbcaea3606e10ddbd
Sunnice <44739306+Sunnice@users.noreply.github.com>
2020-04-10 10:35:21 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,6 +9,6 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 git clone https://github.com/tzxiaozhen88/luci-app-koolproxyR.git package/koolproxyR
 git clone https://github.com/pymumu/smartdns.git package/smartdns
-git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
+git clone https://github.com/ujincn/luci-app-smartdns-compat.git package/luci-app-smartdns
 git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/adguardhome
