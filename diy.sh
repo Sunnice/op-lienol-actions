83ab560413b6334c04372ce5bfc61a6072a5ea1c
Sunnice <44739306+Sunnice@users.noreply.github.com>
2020-04-11 10:29:25 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -12,11 +12,14 @@ git clone https://github.com/pymumu/smartdns.git package/smartdns
 git clone https://github.com/ujincn/luci-app-smartdns-compat.git package/luci-app-smartdns
 git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/adguardhome
-git clone https://github.com/Lienol/openwrt-OpenAppFilter.git package/OpenAppFilter
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+git clone https://github.com/tty228/luci-app-serverchan.git package/serverchan
+git clone https://github.com/skyformat99/eqos.git package/eqos
+git clone https://github.com/ElonH/Rclone-OpenWrt.git package/rclone
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 git clone https://github.com/Lienol/luci-theme-darkmatter.git package/luci-theme-darkmatter
-git clone https://github.com/rosywrt/luci-theme-rosy.git package/luci-theme-rosy2
-git clone https://github.com/Mrbai98/luci-theme-atmaterial.git package/luci-theme-atmaterial2
-git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcatnew
-git clone https://github.com/Leo-Jo-My/luci-theme-opentomato.git package/luci-theme-opentomatonew
+git clone https://github.com/rosywrt/luci-theme-rosy.git package/luci-theme-rosy
+git clone https://github.com/Mrbai98/luci-theme-atmaterial.git package/luci-theme-atmaterial
+git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
+git clone https://github.com/Leo-Jo-My/luci-theme-opentomato.git package/luci-theme-opentomato
 git clone https://github.com/Leo-Jo-My/luci-theme-Butterfly-dark.git package/luci-theme-Butterfly-dark
-git clone https://github.com/kenzok8/openwrt-packages.git package/kenzok8
